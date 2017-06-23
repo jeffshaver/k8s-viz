4fdbac22213b700a47641724958928e3c61a9b97
Jeffrey Shaver <jeff.e.shaver@gmail.com>
2017-06-23 14:05:21 -0400

    fix issues

--- client/src/generate-nodes-and-links.js
+++ client/src/generate-nodes-and-links.js
@@ -1,6 +1,43 @@
 import getLatestDeployments from './get-latest-deployments'
 import {height, width} from './constants'
 
+const findDeploymentNode = (deploymentNodes, pod) => {
+  const deploymentNode = deploymentNodes.find((deploymentNode) => {
+    const {ownerReferences} = pod.metadata
+    const ownerReference = ownerReferences[0]
+
+    if (!ownerReference) {
+      return
+    }
+
+    return pod.metadata.namespace + '_' + ownerReference.name === deploymentNode.id
+  })
+
+  return deploymentNode
+}
+
+const findNamespaceNode = (namespaceNodes, pod) => {
+  const namespaceNode = namespaceNodes.find((namespaceNode) => {
+    return pod.metadata.namespace === namespaceNode.id
+  })
+
+  return namespaceNode
+}
+
+const findAttachedNode = (deploymentNodes, namespaceNodes, pod) => {
+  let attachedNode = findDeploymentNode(deploymentNodes, pod)
+  let attachedNodeIsNamespace = false
+
+  if (attachedNode) {
+    return {attachedNode, attachedNodeIsNamespace}
+  }
+
+  attachedNode = findNamespaceNode(namespaceNodes, pod)
+  attachedNodeIsNamespace = !!attachedNode
+
+  return {attachedNode, attachedNodeIsNamespace}
+}
+
 const generateNodesAndLinks = ({namespaces, deployments, pods}) => {
   let groupNumber = 1
   const cx = width / 2
@@ -83,30 +120,26 @@ const generateNodesAndLinks = ({namespaces, deployments, pods}) => {
       reason = pod.status.containerStatuses[0].state[status].reason
     }
 
-    const deploymentNode = deploymentNodes.find((deploymentNode) => {
-      const {ownerReference} = pod.metadata
+    const {attachedNode, attachedNodeIsNamespace} = findAttachedNode(deploymentNodes, namespaceNodes, pod)
 
-      if (!ownerReference) {
-        return
-      }
-
-      return pod.metadata.namespace + '_' + ownerReference.name === deploymentNode.id
-    })
-
-    if (!deploymentNode) {
+    if (!attachedNode) {
       return
     }
 
     const podNode = {
-      id: deploymentNode.id + '_' + pod.metadata.name,
+      id: attachedNode.id + '_' + pod.metadata.name,
       name: pod.metadata.name,
-      group: deploymentNode.group,
+      group: attachedNode.group,
       status,
       tooltip: {
         Type: 'pod',
         Name: pod.metadata.name,
-        Namespace: deploymentNode.id.split('_')[0],
-        Deployment: deploymentNode.id.split('_')[1],
+        Namespace: attachedNodeIsNamespace
+          ? attachedNode.id
+          : attachedNode.id.split('_')[0],
+        Deployment: attachedNodeIsNamespace
+          ? 'N/A'
+          : attachedNode.id.split('_')[1],
         Status: status + (!reason ? '' : `: ${reason}`)
       }
     }
